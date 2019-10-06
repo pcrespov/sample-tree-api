18139965801b7a2c2eb2703cd406d3e404ecc6e0
Pedro Crespo <crespo@itis.ethz.ch>
2019-10-06 19:47:41 +0200

    draft tests

--- tests/test_it.py
+++ tests/test_it.py
@@ -2,6 +2,9 @@ from treelib import Node, Tree
 import json
 import pytest
 
+import uuid as uuidlib
+
+
 
 @pytest.fixture
 def sample_model_tree():
@@ -10,17 +13,34 @@ def sample_model_tree():
     tree.create_node("Model", 0) # Root
     tree.create_node("Grid", parent=0) # status = Active
     tree.create_node("Block 1", parent=0)
-    tree.create_node("Group 1", 1, parent=0)
-    tree.create_node("Sphere 1", parent=1)
-    tree.create_node("Cylinder 1", parent=1)
+    group1 = tree.create_node("Group 1", parent=0)
+    tree.create_node("Sphere 1", parent=group1.identifier)
+    tree.create_node("Cylinder 1", parent=group1.identifier)
     return tree
 
 
 def test_get_domain(sample_model_tree):
     
-    tree.show()
-
-
+    tree = sample_model_tree
 
     import pdb; pdb.set_trace()
+
+    tree.show()
+    
     print(json.dumps(tree.to_json(), indent=2))
+
+
+    # get root
+    root = tree.nodes[0]
+    assert root.identifier == tree.root
+    
+
+    # get 
+    
+
+
+
+    
+
+
+
