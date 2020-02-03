e8df59625ba3722472708bd9675204162cd3b833
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-03 20:21:22 +0100

    formatting

--- tree-api/tests/__init__.py
+++ tree-api/tests/__init__.py
@@ -3,7 +3,8 @@
 import sys
 from pathlib import Path
 
-current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+current_dir = Path(sys.argv[0] if __name__ ==
+                   "__main__" else __file__).resolve().parent
 repo_dir = (current_dir / "..").resolve()
 
 
